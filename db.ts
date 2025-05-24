51bc1045ae57faefc5a2f9f43b14e9fbdc8cfed4
Gregorius Setiadharma <gregoriussdh@gmail.com>
2025-05-24 09:25:04 +0700

    docker fix

--- db.ts
+++ db.ts
@@ -1,10 +1,10 @@
 import { Client } from "https://deno.land/x/mysql@v2.12.1/mod.ts";
 
-const dbHost = "localhost";
-const dbPort = "3306";
-const dbUser = "root";
-const dbPassword = "";
-const dbName = "chat_app";
+const dbHost = Deno.env.get("DB_HOST") || "localhost";
+const dbPort = parseInt(Deno.env.get("DB_PORT") || "3306", 10);
+const dbUser = Deno.env.get("DB_USER") || "root";
+const dbPassword = Deno.env.get("DB_PASSWORD") || "";
+const dbName = Deno.env.get("DB_NAME") || "chat_app";
 
 const client = await new Client().connect({
   hostname: dbHost,
